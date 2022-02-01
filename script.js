ab6e57f4295e2076d40b4f2edb0f3a87d6a6edf3
Radhika <cu9rad@gmail.com>
2022-02-01 11:51:56 -0500

    initial api request

--- script.js
+++ script.js
@@ -9,4 +9,15 @@ const fivedayHeader = document.querySelector("#fiveDay")
 
 //storing API key
 var APIKey = "dc33b13c1b1dacc4cd49e870ce1ff700";
+
+const getWeather=(city)=>{
+    const apiUrlCoords = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=dc33b13c1b1dacc4cd49e870ce1ff700";
+    fetch(apiUrlCoords)
+    .then(function (response){
+        return response.json()
+        .then(function(data){
+            
+        })
+    }
+}
  
