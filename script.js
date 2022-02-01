99f9b62a8ffa242c867528335e344b685f55c434
Radhika <cu9rad@gmail.com>
2022-02-01 11:48:27 -0500

    initial script file added

---
+++ script.js
@@ -0,0 +1,12 @@
+//getting DOM elements
+const searchForm = document.querySelector("#search-form")
+const cityName = document.querySelector("#city-name")
+const preSearchBtn = document.querySelector("#search")
+const currentWeatherUl = document.querySelector("#forecastNow #currentDetails")
+const preSearchContainer = document.querySelector("#preSearch")
+const fiveDayConatainer =document.querySelector("#fivedayForecast")
+const fivedayHeader = document.querySelector("#fiveDay")
+
+//storing API key
+var APIKey = "dc33b13c1b1dacc4cd49e870ce1ff700";
+ 
